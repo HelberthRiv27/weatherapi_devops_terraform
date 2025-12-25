9e64bd0255a7d2e3348bb561aafad8998b868bb3
HelberthRiv27 <211886668+HelberthRiv27@users.noreply.github.com>
2025-12-24 20:21:05 -0600

    terraform and pipeline yaml

---
+++ main.tf
@@ -0,0 +1,51 @@
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "weatherapi_storage_rg"
+        storage_account_name = "weatherapiterraftstorage"
+        container_name       = "tfstatefile"
+        key                  = "terraform.tfstate"
+        }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-rg"
+  location = "East US"
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "weatherapi"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_address_type     = "Public"
+  dns_name_label      = "weatherapicontainerhelberth27"
+  os_type             = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "helriper27/weatherapi"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 8080
+      protocol = "TCP"
+    }
+  }
+}
+
